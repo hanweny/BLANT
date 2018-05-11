f885d1a50894cf6c3920b8a7afba532842252285
Henry Ye <hanweny@uci.edu>
2018-05-11 00:22:27 -0700

    Deleted SRC code and Added Connection Option

--- R/blant.R
+++ R/blant.R
@@ -1,45 +1,63 @@
 #' The BLANT function
 #'
-#' This function will return a table containing the sampling results\cr
-#' Note:  1. Nodes must be integers numbered 0 through n-1, inclusive.\cr
+#'This function will return a table containing the sampling results\cr\cr\cr
+#' NOTE: \cr\cr
+#' Before using this package, MAKE SURE you have a working Blant function installed\cr\cr
+#' 1. Nodes must be integers numbered 0 through n-1, inclusive.\cr\cr
 #' 2. Duplicates and self-loops should be removed before calling BLANT.
+#' @param filepath: The path to your BLANT function
 #' @param mode --output Mode: o (ODV, the default); i (indexGraphlets); g (GDV); f (graphletFrequency)
 #' @param n -- Sampling number
 #' @param k --The number of nodes in graphlets to be sampled
 #' @param filename --Graph must be in one of the following formats with its extension name:
 #' GML (.gml) GraphML (.xml) LGF(.lgf) CSV(.csv) LEDA(.leda) Edgelist (.el) .
-#' @param sMethod -- Sampling Mehtods. These are the valid Sampling Methods: \cr
-#' 1.   SAMPLE_UNBIASED/SU -- makes things REALLY REALLY slow.  Like 10-100 samples per second rather than a million.\cr
-#' 2.   SAMPLE_NODE_EXPANSION/SNE -- sample using uniform node expansion; about 100,000 samples per second.\cr
-#' 3.   SAMPLE_EDGE_EXPANSION/SEE -- Fastest, up to a million samples per second.\cr
-#' 4.   SAMPLE_RESERVOIR -- Lu Bressan's reservoir sampler, reasonably but not entirely unbiased.\cr
+#' @param connected -- if it is TRUE/T, blant function will return a table ONLY contains connected graphlets.
+#' Otherwise, it can contain both connected and disconnected graphlets.
 #' @keywords blant
+#' @examples
+#' blant(filepath=..., mode="f", n=10000, k=5, filename="syeast.el", connected=T)
 #' @export
-blant <- function(mode, n, k, filename, sMethod, threads = -1) {
-  blantHelper <- function(mode, n, k, filename, sMethod, threads = -1) {
-    mode <- paste("-m", mode, sep="")
-    if(sMethod=="SAMPLE_UNBIASED" || sMethod=="SU") {
-      cmd <- paste("./blantSU", mode, "-s", format(n, scientific = F), "-k", toString(k), filename)
-    } else if(sMethod=="SAMPLE_NODE_EXPANSION" || sMethod=="SNE") {
-      cmd <- paste("./blantSNE", mode, "-s", format(n, scientific = F), "-k", toString(k), filename)
-    } else if(sMethod=="SAMPLE_EDGE_EXPANSION" || sMethod=="SEE") {
-      cmd <- paste("./blantSEE", mode, "-s", format(n, scientific = F), "-k", toString(k), filename)
-    } else if(sMethod=="SAMPLE_RESERVOIRN" || sMethod=="SR") {
-      cmd <- paste("./blantSR", mode, "-s", format(n, scientific = F), "-k", toString(k), filename)
-    }
-    sprintf(cmd)
-    system(cmd, intern=TRUE)
-  }
+blant <- function(filepath,mode, n, k, filename, connected, threads = -1) {
   path = getwd()
-  newpath = paste(installed.packages()["BLANT", "LibPath"],"/BLANT/blantsrc", sep="")
-  setwd(newpath)
-  a = blantHelper(mode, n, k, filename, sMethod, threads)
+  setwd(filepath)
+  a = blantHelper(mode, n, k, filename, threads)
   number = as.numeric(lapply(strsplit(a, " "), function(x) x[1]))
   can = as.numeric(lapply(strsplit(a, " "), function(x) x[2]))
   df = data.frame(v1=can, v2=number)
   names(df) = c("Graphlet Number", "Count")
+  if(connected) {df=ConnectedHelper(k,df)}
   setwd(path)
   return(df)
 }
 
-#blant("f", 10000, 5, "syeast.el","SNE")
+blantHelper <- function(mode, n, k, filename, threads = -1) {
+  mode <- paste("-m", mode, sep="")
+  cmd <- paste("./blant", mode, "-s", format(n, scientific = F), "-k", toString(k), filename)
+  sprintf(cmd)
+  system(cmd, intern=TRUE)
+}
+
+ConnectedHelper<-function(k,a) {
+  path=getwd()
+  setwd(paste(installed.packages()["BLANT", "LibPath"],"/BLANT/blantsrc", sep=""))
+  maps <-read.table(paste("UpperToLower",toString(k),".txt",sep=""))
+  maps <- maps[order(maps$V5),]
+  rownames(maps) <- seq(length=nrow(maps))
+  toDelete=c()
+  for(i in seq(length=nrow(maps))) {
+    if(maps[i,"V1"] == 0) {
+      toDelete <- c(toDelete,i)
+    }
+  }
+  a = a[-toDelete,]
+  rownames(a) <- seq(length=nrow(a))
+  setwd(path)
+  return(a)
+}
+
+
+#blant("~/research/wayne/BLANT","f", 10000, 5, "syeast.el",T)
+
+
+
+
